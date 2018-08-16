fb66dfaa4927bddbb5f4d12bf3292115d6c282d2
Jacob Fisher <jacobfisher18@gmail.com>
2018-08-16 12:02:01 -0700

    created Calculator component

---
+++ src/Components/Calculator.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class Calculator extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      
+    }
+  }
+
+  render() {
+    return (
+      <div className="Calculator">
+        <p>content goes here</p>
+      </div>
+    );
+  }
+}
+
+export default Calculator;
